a588392746b5fe2fd7fb6ef92c82fe91efc66ac2
Ira Matsuk <g3726866@gmail.com>
2020-03-30 14:41:44 +0300

    feat: complete task 11 from article 2.3

---
+++ FundamentalsOfAlgorithmizationAndSoftwareDesign/Homework/section2/article2.3_task11/article2.3_task11/article2.3_task11.cpp
@@ -0,0 +1,37 @@
+﻿// article2.3_task11.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#define Pi 3.1415
+
+double coneLateralSurfaceArea(double r, double l)
+{
+    return Pi * r * l;
+}
+
+double coneTotalSurfaceArea(double r, double l)
+{
+    return Pi * r * (r + l);
+}
+
+double coneVolume(double r, double h)
+{
+    return (h / 3.0) * Pi * pow(r, 2);
+}
+
+
+int main()
+{
+    double radius = 0, length = 0, height = 0;
+
+    printf("Enter the radius of the cone:\n");
+    scanf_s("%lf", &radius);
+    printf("Enter the length of the edge of the cone:\n");
+    scanf_s("%lf", &length);
+    printf("Enter the height of the cone:\n");
+    scanf_s("%lf", &height);
+    printf("The cone lateral surface area is %.3lf\n", coneLateralSurfaceArea(radius, length));
+    printf("The total surface area of ​​the cone is %.3lf\n", coneTotalSurfaceArea(radius, length));
+    printf("The cone volume is %.3lf\n", coneVolume(radius, height));
+    return 0;
+ }
