b64e36a9a057a4f63211aa72cc5a882267f6a120
Ira Matsuk <g3726866@gmail.com>
2020-04-08 17:31:03 +0300

    feat: complete task 6 article 2.3 classwork

---
+++ FundamentalsOfAlgorithmizationAndSoftwareDesign/Classwork/section2/article2.3_task6/article2.3_task6/article2.3_task6.cpp
@@ -0,0 +1,11 @@
+// article2.3_task6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+// Пример №6. Еще один пример кода с другим стилем комментариев
+#include <stdio.h>
+
+int main()
+{
+    printf("I like these new comments!"); //Простое выражение
+    return 0;
+}
+
