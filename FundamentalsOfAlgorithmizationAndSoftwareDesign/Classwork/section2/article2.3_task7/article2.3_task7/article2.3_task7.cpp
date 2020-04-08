c3ea59f6a625c32ce7ff4b5c9e7193056e70e02b
Ira Matsuk <g3726866@gmail.com>
2020-04-08 17:31:51 +0300

    feat: complete task 7 article 2.3 classwork

---
+++ FundamentalsOfAlgorithmizationAndSoftwareDesign/Classwork/section2/article2.3_task7/article2.3_task7/article2.3_task7.cpp
@@ -0,0 +1,17 @@
+// article2.3_task7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+// Пример №7. Использование управляющих последовательностей
+
+#include <stdio.h>
+
+int main()
+{
+	/* Эти три строки демонстрируют использование
+	самых популярных управляющих последовательностей */
+	printf("Column A\tColumn B\tColumn C");
+	printf("\nMy Computer\'s Beep Sounds Like This: \a!\n");
+	printf("\"Letz\bs fix that typo and then show the backslash ");
+	printf("character \\\" she said\n");
+	return 0;
+}
+
