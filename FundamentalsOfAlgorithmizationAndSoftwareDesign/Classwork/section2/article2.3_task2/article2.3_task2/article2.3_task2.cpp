ed8d2cca361228936be7ab0e663553c919c49f60
Ira Matsuk <g3726866@gmail.com>
2020-04-08 17:28:20 +0300

    feat: complete task 1 article 2.3 classwork

---
+++ FundamentalsOfAlgorithmizationAndSoftwareDesign/Classwork/section2/article2.3_task2/article2.3_task2/article2.3_task2.cpp
@@ -0,0 +1,16 @@
+// article2.3_task2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+#include<locale.h>
+#define FALSE 0
+void printGreeting();
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int bet;
+    int bank = 100;
+    char suit, rank, stillPlay;
+    printf("Старшая карта\n\n");
+    return 0;
+}
