d6332e8c4042d4d404d0606a5b74b0dae3e75831
Ira Matsuk <g3726866@gmail.com>
2020-04-18 01:50:15 +0300

    feat: implemented task calculator from task5 lab2

---
+++ InternetTechnology/lab2/task6/script.js
@@ -0,0 +1,12 @@
+function calcSum(){
+    result.value = box1.value + box2.value;
+}
+function calcMinus(){
+    result.value = box1.value - box2.value;
+}
+function calcMult(){
+    result.value = box1.value * box2.value;
+}
+function calcDiv(){
+    result.value = box1.value / box2.value;
+}
