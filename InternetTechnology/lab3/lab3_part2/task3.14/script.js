4c1e7399c4dddf38b753d0c4612c1c0425bfbec1
Ira Matsuk <g3726866@gmail.com>
2020-05-31 09:24:35 +0300

    feat: complete task3.14 lab3 part2

---
+++ InternetTechnology/lab3/lab3_part2/task3.14/script.js
@@ -0,0 +1,12 @@
+function takeOffBox()  {
+    document.getElementById("box_1").style.display="none";
+}
+
+function hiddenBox() {
+    document.getElementById("box_2").style.visibility = "hidden";
+}
+
+function openBox() {
+    document.getElementById("box_1").style.display="block";
+    document.getElementById("box_2").style.visibility="visible";
+}
