85eaa14a86c6fbc22b06b9286dde76163e45a19f
Ira Matsuk <g3726866@gmail.com>
2020-07-21 15:38:30 +0300

    feat: complete task 1 webinar 6

---
+++ InternetTechnology/webinar6/task1/script.js
@@ -0,0 +1,11 @@
+'use strict';
+var age = prompt("Ваш возраст", "18");
+switch(age) {
+    case 18:
+        alert("Никогда не срабатывает");
+    case "18":
+        alert("Вам 18 лет");
+        break;
+    default:
+        alert("Любое значение, не совпавшее в case");        
+}
