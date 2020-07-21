2615776b6d8f71316ad4fbd7b1f736dc5402a47e
Ira Matsuk <g3726866@gmail.com>
2020-07-21 11:51:05 +0300

    feat: complete task 9_2 webinar 6

---
+++ InternetTechnology/webinar6/task9_2/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//Solution №1
+var price1 = 0.1, price2 = 0.2;
+alert( +(price1 + price2).toFixed(2) + "$");
+
+
+//Solution №2
+// var price1 = 10, price2 = 20;
+// alert( +(price1 + price2)/100 + "$");
