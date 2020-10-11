90a34c919302fe1586ffd51677beb6425a1ad687
IraMatsuk <g3726866@gmail.com>
2020-10-11 16:21:40 +0300

    feat: complete task5 lab1 part1

---
+++ WEB/lab1_part1/task5/script.js
@@ -0,0 +1,8 @@
+function getDates(element) {
+    date1 = new Date(388048292221);
+    date2 = new Date(734019522932);
+    date3 = new Date(983283741123);
+    document.write(date1 + '<br />');
+    document.write(date2 + '<br />');
+    document.write(date3);
+}
