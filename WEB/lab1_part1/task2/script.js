5b7531cc5ec3c80dfaf1a86e4faa85bd0657abd8
IraMatsuk <g3726866@gmail.com>
2020-10-11 15:16:28 +0300

    feat: solve task 2 lab1 part1 where use substr() and substring() methods

---
+++ WEB/lab1_part1/task2/script.js
@@ -0,0 +1,15 @@
+function getSubstr(in_element, out_element) {
+    var node = document.getElementById(in_element);  
+    var beginIndexOfBoldStr = node.innerHTML.indexOf("<b>") + 3;
+    var endIndexOfBoldStr = node.innerHTML.indexOf("</b>")
+    var substring = node.innerHTML.substr(beginIndexOfBoldStr, endIndexOfBoldStr - beginIndexOfBoldStr);
+    document.getElementById(out_element).innerHTML = substring;
+}
+
+function getSubstring(in_element, out_element) {
+    var node = document.getElementById(in_element);  
+    var beginIndexOfBoldStr = node.innerHTML.indexOf("<b>") + 3;
+    var endIndexOfBoldStr = node.innerHTML.indexOf("</b>")
+    var substring = node.innerHTML.substring(beginIndexOfBoldStr, endIndexOfBoldStr);
+    document.getElementById(out_element).innerHTML = substring;
+}
