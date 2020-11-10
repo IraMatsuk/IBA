52626b0034d300a80db2f6b8fe6ec8c136cdd776
IraMatsuk <g3726866@gmail.com>
2020-11-10 23:22:05 +0300

    feat: complete task2 lab 5 using bootstrap4

--- WEB/lab1_part1/task2/script.js
+++ WEB/lab1_part1/task2/script.js
@@ -6,10 +6,10 @@ function getSubstr(in_element, out_element) {
     document.getElementById(out_element).innerHTML = substring;
 }
 
-function getSubstring(in_element, out_element) {
+function getSubstring(in_element, out_element1) {
     var node = document.getElementById(in_element);  
     var beginIndexOfBoldStr = node.innerHTML.indexOf("<b>") + 3;
     var endIndexOfBoldStr = node.innerHTML.indexOf("</b>")
     var substring = node.innerHTML.substring(beginIndexOfBoldStr, endIndexOfBoldStr);
-    document.getElementById(out_element).innerHTML = substring;
+    document.getElementById(out_element1).innerHTML = substring;
 }
