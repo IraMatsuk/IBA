0cb58d1feb820490e0ec14e448a619e8d254d50d
IraMatsuk <g3726866@gmail.com>
2021-07-06 21:46:31 +0300

    feat: add constructor and annotation for constructors

--- PRILIS/sf-pet-clinic/pet-clinic-data/src/main/java/by/matsuk/sfpetclinic/model/Person.java
+++ PRILIS/sf-pet-clinic/pet-clinic-data/src/main/java/by/matsuk/sfpetclinic/model/Person.java
@@ -1,13 +1,30 @@
 package by.matsuk.sfpetclinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 
-@Data
+/*@Data
 @MappedSuperclass
 public class Person extends BaseEntity{
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+}*/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public class Person extends BaseEntity{
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
     @Column(name="first_name")
     private String firstName;
     @Column(name="last_name")
